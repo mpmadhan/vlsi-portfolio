68b6a2896c9787a72070116917b17daa2af70bae
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-04 20:42:57 +0530

    Create full_adder_tb.v

---
+++ 01-Combinational/02-Full_Adder/full_adder_tb.v
@@ -0,0 +1,31 @@
+//Testbench for Full Adder
+`timescale 1ns/1ps
+module full_adder_tb();
+  //1. Signal declaration
+  reg a, b, cin;
+  wire sum, cout;
+  //2. DUT instantiation
+  full_adder dut(.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+  //3. Stimulus + Waveform
+  initial begin
+    //3.1 Waveform
+    $dumpfile("full_adder_tb.vcd");
+    $dumpvars(0,full_adder_tb);
+    //3.2 Stimulus
+    $display("Time | A B Cin | Sum  Cout |");
+    $display("-----|---------|----------|");
+    a=0; b=0; cin=0; #10;
+    a=0; b=0; cin=1; #10;
+    a=0; b=1; cin=0; #10;
+    a=0; b=1; cin=1; #10;
+    a=1; b=0; cin=0; #10;
+    a=1; b=0; cin=1; #10;
+    a=1; b=1; cin=0; #10;
+    a=1; b=1; cin=1; #10;
+    $finish;
+  end
+  //4. Observation
+  initial begin
+    $monitor("%4t | %b %b %b | %b   %b |", $time, a, b, cin, sum, cout);
+  end
+endmodule
