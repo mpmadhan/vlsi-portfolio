732bcd4955192aa69ae50b1142257c616301a94e
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-24 15:41:02 +0530

    Update rca_4bit_tb.v

--- 01-Combinational/03-RCA/rca_4bit_tb.v
+++ 01-Combinational/03-RCA/rca_4bit_tb.v
@@ -2,23 +2,28 @@
 `timescale 1ns/1ps
 module rca_4bit_tb();
   //1.Signal declaration
-  reg [4:0] a,b;
+  reg [3:0] a,b;
   reg cin;
-  wire [4:0] sum;
+  wire [3:0] sum;
   wire cout;
   //2.DUT instantiation
   rca_4bit dut(.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
-  //3.Stimulus
+  //3.Stimulus and Waveform
   initial begin
-    #10;
-    a=0;b=0;cin=0; #10;
-    a=0;b=0;cin=1; #10;
-    a=0;b=1;cin=0; #10;
-    a=0;b=1;cin=1; #10;
-    a=1;b=0;cin=0; #10;
-    a=1;b=0;cin=1; #10;
-    a=1;b=1;cin=0; #10;
-    a=1;b=1;cin=1; #10;
-    #100 $finish;
+    //Waveform analysis
+    $dumpfile("rca_4bit_tb.vcd");
+    $dumpvars(0,rca_4bit_tb);
+    //Stimulus
+    $display("| Time | A B Cin | Sum Cout |");
+    $display("|------|---------|----------|");
+    a=4'd0;b=4'd0;cin=0; #10; //0+0+0 = 0
+    a=4'd3;b=4'd4;cin=1; #10; //3+4+1 = 8
+    a=4'd5;b=4'd10;cin=1; #10; //5+10+1 = 16 overflow
+    a=4'd15;b=4'd15;cin=1; #10; //15+15+1 = 31 overflow
+    $finish;
+  end
+  //4.Observation
+  initial begin
+    $monitor("| %4t | %4b %4b %b | %4b %b |",$time,a,b,cin,sum,cout);
   end
 endmodule
