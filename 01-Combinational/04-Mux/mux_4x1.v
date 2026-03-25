5c479ddbf3dc1c003474353d25d53316214a677d
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-25 13:12:12 +0530

    Create mux_4x1.v

---
+++ 01-Combinational/04-Mux/mux_4x1.v
@@ -0,0 +1,16 @@
+//RTL Code for 4x1 Multiplexor
+module mux_4x1(
+  input i0,i1,i2,i3,
+  input [1:0] sel,
+  output reg y
+);
+  always @(*) begin
+    case (sel)
+      2'b00 : y=i0;
+      2'b01 : y=i1;
+      2'b10 : y=i2;
+      2'b11 : y=i3;
+      default : y=1'bx;
+    endcase
+  end
+endmodule
