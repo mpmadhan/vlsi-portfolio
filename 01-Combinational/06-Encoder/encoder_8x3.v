297cb5d91e9ba7a31f383d02ad6dcc78c22c202c
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-27 21:41:06 +0530

    Create encoder_8x3.v

---
+++ 01-Combinational/06-Encoder/encoder_8x3.v
@@ -0,0 +1,20 @@
+//RTL Code for 8x3 Encoder
+/*
+I7 I6 I5 I4 I3 I2 I1 I0 | Y2 Y1 Y0
+0  0  0  0  0  0  0  1  | 0  0  0
+0  0  0  0  0  0  1  0  | 0  0  1
+0  0  0  0  0  1  0  0  | 0  1  0
+0  0  0  0  1  0  0  0  | 0  1  1
+0  0  0  1  0  0  0  0  | 1  0  0
+0  0  1  0  0  0  0  0  | 1  0  1
+0  1  0  0  0  0  0  0  | 1  1  0
+1  0  0  0  0  0  0  0  | 1  1  1
+*/
+module encoder_8x3(
+  input [7:0] in,
+  output [2:0] out
+);
+  assign Y2 = (I7 | I6 | I5 | I4);
+  assign Y1 = (I2 | I3 | I6 | I7);
+  assign Y0 = (I1 | I3 | I5 | I7);
+endmodule
