e64c8988c5ad5360086aa542e019f4b86ac38bd7
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-27 21:53:10 +0530

    Update encoder_8x3.v

--- 01-Combinational/06-Encoder/encoder_8x3.v
+++ 01-Combinational/06-Encoder/encoder_8x3.v
@@ -14,7 +14,7 @@ module encoder_8x3(
   input [7:0] in,
   output [2:0] out
 );
-  assign out[2] = (I7 | I6 | I5 | I4);
-  assign out[1] = (I2 | I3 | I6 | I7);
-  assign out[0] = (I1 | I3 | I5 | I7);
+  assign out[2] = (in[7] | in[6] | in[5] | in[4]);
+  assign out[1] = (in[7] | in[6] | in[3] | in[2]);
+  assign out[0] = (in[7] | in[5] | in[3] | in[1]);
 endmodule
