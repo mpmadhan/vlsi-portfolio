b5238ce4f58dd170bee54996299ef7d354dd14b1
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-30 15:42:38 +0530

    Create parity_checker.v

---
+++ 01-Combinational/10-Parity Bit/parity_checker.v
@@ -0,0 +1,9 @@
+//RTL Code for 4-bit Parity Checker
+module parity_checker(
+  input [3:0] d,
+  input parity,
+  output error
+);
+  assign error = (d[3]^d[2]^d[1]^d[0]^parity);
+  //if error = 1, then error in data
+endmodule
