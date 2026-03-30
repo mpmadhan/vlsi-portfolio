e54fd6306eb8b5a3aacca83daa8ad509021875b4
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-30 16:09:11 +0530

    Create parity_generator_tb.v

---
+++ 01-Combinational/10-Parity Bit/parity_generator_tb.v
@@ -0,0 +1,27 @@
+//Testbench for 4-Bit Parity Generator
+`timescale 1ns/1ps
+module parity_generator_tb();
+  //1. Signal declaration
+  reg [3:0] d;
+  wire parity;
+  integer i;
+  //2. DUT instantiation
+  parity_generator dut(.d(d),.parity(parity));
+  //3. waveform + Stimulus
+  initial begin
+    //3.1 Waveform
+    $dumpfile ("parity_generator_tb.vcd");
+    $dumpvars (0,parity_generator_tb);
+    //3.2 Display
+    $display("Time |  D  | Parity");
+    $display("-----|-----|-------");
+    //3.3 Stimulus
+    for (i=0;i<16;i=i+1) begin
+      d=i; #10;
+    end
+  end
+  //4. Observation
+  initial begin
+    $monitor("%4t | %4b | %b",$time,d,parity);
+  end
+endmodule
