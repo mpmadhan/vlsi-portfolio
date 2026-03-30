b94a8c8a38bb94163b02d0668afedbb46db0a94a
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-30 15:36:29 +0530

    Create parity_generator.v

---
+++ 01-Combinational/10-Parity Bit/parity_generator.v
@@ -0,0 +1,8 @@
+//RTL Code for 4-Bit Parity Generator Circuit
+module parity_generator(
+  input [3:0] d,
+  output parity
+);
+  //Generating Parity Bit (Output 0 if even no. of 1's)
+  assign parity = (d[3] ^ d[2] ^ d[1] ^ d[0]);
+endmodule
