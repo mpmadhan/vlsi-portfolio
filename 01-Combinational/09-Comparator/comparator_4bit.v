9212900deb8549af6ca4096ddbca4b10e3d3eda6
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-30 11:09:38 +0530

    Create comparator_4bit.v

---
+++ 01-Combinational/09-Comparator/comparator_4bit.v
@@ -0,0 +1,10 @@
+//RTL Code for 4-Bit Comparator
+module comparator_4bit(
+  input [3:0] a,b,
+  output eq,gt,lt
+);
+  assign eq = (a==b);
+  assign gt = (a > b);
+  assign lt = (a < b);
+endmodule
+  
