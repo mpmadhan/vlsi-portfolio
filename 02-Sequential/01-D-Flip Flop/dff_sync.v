251461d7a0cda3eba3c7c172731cf301e01ba63d
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-31 12:08:03 +0530

    Update dff_sync.v

--- 02-Sequential/01-D-Flip Flop/dff_sync.v
+++ 02-Sequential/01-D-Flip Flop/dff_sync.v
@@ -1,10 +1,10 @@
 //RTL Code for Synchronous D-Flip flop
 module dff_sync(
-  input clk, d, rst,
+  input clk, d, reset,
   output reg q
 );
   always @(posedge clk) begin
-    if(rst)
+    if(reset)
       q<=0;
     else
       q<=d;
