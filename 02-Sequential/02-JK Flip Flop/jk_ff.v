c8c436b35dfe30d5ce7c5955acdba903ce49c1df
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-03-31 15:32:02 +0530

    Update jk_ff.v

--- 02-Sequential/02-JK Flip Flop/jk_ff.v
+++ 02-Sequential/02-JK Flip Flop/jk_ff.v
@@ -1,4 +1,33 @@
 //RTL Code for JK Flip Flop
+/*
+J K | Q 
+0 0 | Q
+0 1 | 0
+1 0 | 1
+1 1 | Q'
+
+J K Q | Qnext
+------|------
+0 0 0 |   0
+0 0 1 |   1
+0 1 0 |   0
+0 1 1 |   0
+1 0 0 |   1
+1 0 1 |   1
+1 1 0 |   1
+1 1 1 |   0
+Qnext = (J'K'Q)|(JK'Q')|(JK'Q)|(JKQ')
+      = K'Q(J|J') | JQ'(K'|K)
+      = J.Q' | K'.Q
+*/
 module jk_ff(
-  input clk,j,k,
+  input clk, j, k, reset,
+  output reg q
 );
+  always @(posedge clk) begin
+    if(reset)
+      q <= 0;
+    else
+      q <= (j&(~q))|((~k)&q);
+  end
+endmodule
