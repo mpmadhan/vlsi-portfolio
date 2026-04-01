3889f99242f71e245eec40fa66b01f882891e9b8
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-04-01 12:58:03 +0530

    Update srff.v

--- 02-Sequential/04-SR Flip Flop/srff.v
+++ 02-Sequential/04-SR Flip Flop/srff.v
@@ -1,11 +1,27 @@
 //RTL Code for SR Flip Flop
 /*
 S R Q | Qnext
-0 0 0 |   
-0 0 1 |   
-0 1 0 |   
-0 1 1 |   
-1 0 0 |   
-1 0 1 |   
-1 1 0 |   
-1 1 1 |   
+------|------
+0 0 0 |   0
+0 0 1 |   1
+0 1 0 |   0
+0 1 1 |   0
+1 0 0 |   1
+1 0 1 |   1
+1 1 0 |   X
+1 1 1 |   X
+Qnext = S | (R'.Q)
+*/
+module srff(
+  input clk, s, r, reset,
+  output q
+);
+  always @(posedge clk) begin
+    if(reset)
+      q <= 0;
+    else if(s&r)
+      q <= 1'bx;
+    else
+      q <= s|((~r)&q);
+  end
+endmodule
