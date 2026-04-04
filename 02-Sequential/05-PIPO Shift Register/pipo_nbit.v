82e7e531ca0cd24d1669065a5bae36fc5bfe0535
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-04-04 15:35:04 +0530

    Update pipo_nbit.v

--- 02-Sequential/05-PIPO Shift Register/pipo_nbit.v
+++ 02-Sequential/05-PIPO Shift Register/pipo_nbit.v
@@ -1,8 +1,8 @@
 //RTL Code for N-Bit PIPO Shift Register
 module pipo_nbit #(parameter N=4)(
   input clk, reset,
-  input [(N-1):0] d;
-  output reg [(N-1):0] q;
+  input [(N-1):0] d,
+  output reg [(N-1):0] q
 );
   always @(posedge clk) begin
     if(reset)
