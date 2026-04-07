76b847422b4f4ee26fa8deef071a7b86665b06fa
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-04-07 16:37:06 +0530

    Create piso_nbit.v

---
+++ 02-Sequential/08-PISO Shift Register/piso_nbit.v
@@ -0,0 +1,17 @@
+//RTL Code for Parallel IN Serial OUT Shift Register
+module piso_nbit #(parameter N=4)(
+  input clk, reset, load,
+  input [(N-1):0] d,
+  output q
+);
+  wire [(N-1):0] shift_reg;
+  always @(posedge clk) begin
+    if(reset)
+      shift_reg <= 0;
+    else if(load)
+      shift_reg <= d;
+    else
+      shift_reg <= {shift_reg[(N-2):0],1'b0};
+  end
+  assign d = shift_reg[N-1];
+endmodule
