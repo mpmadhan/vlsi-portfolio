b970f5cf93f24c8c611b02fd323aff8090fa882c
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-04-09 12:12:36 +0530

    Create upcounter_nbit.v

---
+++ 02-Sequential/10-N-Bit Up Counter/upcounter_nbit.v
@@ -0,0 +1,13 @@
+//RTL Code for N-Bit UP- Counter
+module upcounter_nbit #(parameter N=4)(
+  input clk, reset, enable,
+  output [(N-1):0] q
+);
+  always @(posedge clk) begin
+    if(reset)
+      q<=0;
+    else if(enable)
+      q <= q+1;
+    //if reset and enable is 0, count holds
+  end
+endmodule
