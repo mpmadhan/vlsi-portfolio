610b2bb0a00e9e8d14bd06197dbcc74cbb73d415
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-04-10 10:29:10 +0530

    Update modn_counter.v

--- 02-Sequential/13-Mod N Counter/modn_counter.v
+++ 02-Sequential/13-Mod N Counter/modn_counter.v
@@ -1,5 +1,20 @@
 //RTL Code for Mod-N Counter
-module modn_counter #(parameter N=10)(
+module modn_counter #(
+  parameter N=4,
+  parameter M=10)(
   input clk, reset, enable,
-  output [//(N-1):0]
+  output [(N-1):0] q
 );
+  always @(posedge clk) begin
+    if(reset)
+      q<=0;
+    else if(enable) begin
+      if(q=(M-1))
+        q<=0;
+      else
+        q<=q+1;
+    end
+    else
+      q<=q;
+  end
+endmodule
