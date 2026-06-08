3fecdfd895e35d0fd41f4dc296999bbdd5c4abc8
Madhan M.P <mpmadhan04@gmail.com>
2026-06-08 07:44:11 +0530

    UART Receiver completed

--- 03-Projects/03-UART/uart_rx.v
+++ 03-Projects/03-UART/uart_rx.v
@@ -7,83 +7,63 @@ module uart_rx(
   output reg [7:0] data_out
 );
   //State Encoding
-  localparam IDLE = 2'b00;
-  localparam START = 2'b01;
-  localparam DATA = 2'b10;
-  localparam STOP = 2'b11;
+  localparam RX_START = 2'b01;
+  localparam RX_DATA = 2'b10;
+  localparam RX_STOP = 2'b11;
   //Registers
-  reg [1:0] state, next_state;
-  reg [2:0] bit_count;
-  reg [3:0] sample_count;
+  reg [1:0] state;
+  reg [2:0] bit_pos;
+  reg [3:0] sample_count = 0;
   reg [7:0] temp_reg;
   //Sequential logic
   always @(posedge clk) begin
     if(reset) begin
-      state <= IDLE;
-      bit_count <= 0;
+      state <= RX_START;
+      bit_pos <= 0;
       sample_count <= 0;
       temp_reg <= 0;
+      ready <= 1'b1;
     end
     else begin
-      state <= next_state;
+      ready <= 1'b0;
       case(state)
-        IDLE: begin
-          sample_count <= 0;
-          bit_count <= 0;
-        end
-        START: begin
-          if(rx_en) begin
-            if(sample_count == 7)
-              sample_count <= 0;
-            else 
+        RX_START: begin
+          if(~rx) begin
+            if(rx_en)
               sample_count <= sample_count + 1'b1;
+            if(sample_count == 15) begin
+              state <= RX_DATA;
+              sample_count <= 0;
+              bit_pos <= 0;
+              temp_reg <= 0;
+            end
           end
+          else
+            sample_count <= 0;
         end
-        DATA: begin
+        RX_DATA: begin
           if(rx_en) begin
             sample_count <= sample_count + 1'b1;
-            if(sample_count == 7)
-              temp_reg <= {rx,temp_reg[7:1]};
+            if(sample_count == 7) begin
+              temp_reg[bit_pos] <= rx;            //Sampling mid bit value for accuracy
+            end
             if(sample_count == 15) begin
-              bit_count <= bit_count + 1'b1;
               sample_count <= 0;
+              bit_pos <= bit_pos + 1'b1;
+            end
+            if(bit_pos == 7 && sample_count == 15) begin
+              state <= RX_STOP;
             end
           end
         end
-        STOP: begin
+        RX_STOP: begin
           if(rx_en) begin
-            sample_count <= sample_count + 1'b1;
-            if(sample_count == 15) begin
-              bit_count <= 0;
-              sample_count <= 0;
-            end
+            data_out <= temp_reg;
+            ready <= 1'b1;
+            state <= RX_START;
           end
         end
       endcase
     end
-  end
-  //Combinational next state logic
-  always @(*) begin
-    case(state)
-      IDLE: next_state = (rx == 0) ? START : IDLE;
-      START: begin
-        if(rx_en && sample_count == 7)
-          next_state = (rx == 0)? DATA : IDLE;
-        else
-          next_state = START;
-      end
-      DATA: next_state = (rx_en && bit_count ==7 && sample_count == 15) ? STOP : DATA;
-      STOP: next_state = (rx_en && sample_count == 15) ? IDLE : STOP;
-      default: next_state = IDLE;
-    endcase
-  end
-  //Combinational output logic
-  always @(*) begin
-    data_out = temp_reg;
-    ready = 1'b0;
-    case(state)
-      STOP: begin ready = 1'b1; end
-      default: begin ready = 1'b0; end
-    endcase
   end
 endmodule
