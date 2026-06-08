b68865668c237053f4f2c85e7a63842175bd51a3
Madhan M.P <mpmadhan04@gmail.com>
2026-06-08 07:44:52 +0530

    UART Top module Completed

--- 03-Projects/03-UART/uart_top.v
+++ 03-Projects/03-UART/uart_top.v
@@ -7,10 +7,8 @@ module uart_top(
 );
   wire tx_en, rx_en;
   wire tx_internal;
-
   baud_rate_generator baud(.clk(clk),.reset(reset),.tx_en(tx_en),.rx_en(rx_en));
   uart_tx transmitter(.clk(clk),.reset(reset),.tx_start(tx_start),.tx_en(tx_en),.data_in(data_in),.tx(tx_internal),.busy(busy));
   uart_rx receiver(.clk(clk),.reset(reset),.rx(tx_internal),.rx_en(rx_en),.ready(ready),.data_out(data_out));
-
   assign tx = tx_internal;
 endmodule
