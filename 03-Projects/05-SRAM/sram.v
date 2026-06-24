7df18946a1da7ff48267a230a9db0d0781589561
Madhan M.P <mpmadhan04@gmail.com>
2026-06-24 08:16:17 +0530

    SRAM Module implemented

--- 03-Projects/05-SRAM/sram.v
+++ 03-Projects/05-SRAM/sram.v
@@ -7,18 +7,22 @@ module sram(
   input o_en,
   input [7:0] addr,
   inout [7:0] data,
-  output ready
+  output reg ready
 );
   //State Encoding
   parameter IDLE = 2'b00;
   parameter WRITE = 2'b01;
   parameter READ = 2'b10;
   parameter DONE = 2'b11;
+  
   //Memory Creation
   reg [7:0] mem [0:255]; 
+  
   //Registers
   reg [1:0] state, next_state;
   reg [7:0] data_out;
+  reg r_en;
+  
   //Reset Logic
   always @(posedge clk or negedge reset_n) begin
     if(!reset_n) 
@@ -26,28 +30,36 @@ module sram(
     else
       state <= next_state;
   end
+  
   //Output Logic
   always @(posedge clk or negedge reset_n) begin
     if(!reset_n) begin
       data_out <= 0;
       ready <= 0;
+      r_en <= 0;
+    end 
+    else begin
+      case(state) 
+        IDLE: begin
+          r_en <= 0;
+          ready <= 0;
+        end
+        WRITE: begin
+          mem[addr] <= data;
+          ready <= 0;
+          r_en <= 0;
+        end
+        READ: begin
+          data_out <= mem[addr];
+          ready <= 0;
+          r_en <= 0;
+        end
+        DONE: begin
+          ready <= 1;
+          r_en <= 1;
+        end 
+      endcase
     end
-    case(state) 
-      IDLE:
-        ready <= 0;
-      WRITE: begin
-        mem[addr] <= data;
-        ready <= 0;
-      end
-      READ: begin
-        data_out <= mem[addr];
-        ready <= 0;
-        data <= data_out;
-      end
-      DONE: begin
-        ready <= 1;
-      end 
-    endcase
   end
   
   //Next State Logic
@@ -68,5 +80,6 @@ module sram(
       default: next_state = IDLE;
     endcase
   end
-  assign data = data_out;
+  //Assigning Read data from memory
+  assign data = (r_en) ? data_out:8'bz;
 endmodule
