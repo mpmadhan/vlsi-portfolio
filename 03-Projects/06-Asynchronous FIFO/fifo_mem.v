6b74459a8f22429541c7f8f4ac2dd2ddd654d3e7
Madhan M.P <mpmadhan04@gmail.com>
2026-08-04 10:09:52 +0530

    Create fifo_mem.v
    FIFO Memory

---
+++ 03-Projects/06-Asynchronous FIFO/fifo_mem.v
@@ -0,0 +1,25 @@
+//FIFO Memory
+module fifo_mem #(
+  parameter DEPTH = 8,
+  parameter DATA_LEN = 8,
+  parameter WIDTH = 3
+)(
+  input wclk,
+  input w_en,
+  input [WIDTH:0] b_wptr,
+  input [WIDTH:0] b_rptr,
+  input [DATA_LEN-1:0] data_in,
+  output [DATA_LEN-1:0] data_out,
+  input full
+);
+  //Memory
+  reg [WIDTH-1:0] fifo [0:DEPTH-1];
+  
+  always @(posedge wclk) begin
+    if(w_en & ~full) begin
+      fifo[b_wptr[WIDTH-1:0]] <= data_in; 
+      //WIDTH-1 because the MSB bit is for checking wrap around we don't need it to write data into fifo
+    end
+  end
+  assign data_out = fifo[b_rptr[WIDTH-1:0]];
+endmodule
