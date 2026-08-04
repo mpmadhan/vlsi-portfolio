f690b4794c1486172941d5b1a912282e9b0e7012
Madhan M.P <mpmadhan04@gmail.com>
2026-08-04 10:18:13 +0530

    Commit fifo_mem.v
    Writing and Reading from FIFO Memory

--- 03-Projects/06-Asynchronous FIFO/fifo_mem.v
+++ 03-Projects/06-Asynchronous FIFO/fifo_mem.v
@@ -8,18 +8,17 @@ module fifo_mem #(
   input w_en,
   input [WIDTH:0] b_wptr,
   input [WIDTH:0] b_rptr,
+  input full,
   input [DATA_LEN-1:0] data_in,
-  output [DATA_LEN-1:0] data_out,
-  input full
+  output [DATA_LEN-1:0] data_out
 );
-  //Memory
+  //Memory creation
   reg [DATA_LEN-1:0] fifo [0:DEPTH-1];
-  
+  //Writing into FIFO
   always @(posedge wclk) begin
-    if(w_en & ~full) begin
-      fifo[b_wptr[WIDTH-1:0]] <= data_in; 
-      //WIDTH-1 because the MSB bit is for checking wrap around we don't need it to write data into fifo
-    end
+    if(w_en & ~full)
+      fifo[b_wptr[WIDTH-1:0]] <= data_in;
   end
+  //Reading from FIFO
   assign data_out = fifo[b_rptr[WIDTH-1:0]];
 endmodule
